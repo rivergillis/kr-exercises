6417fc689f88e2360c1090838e2c8dcb824dd21e
River Gillis <jrivergillis@gmail.com>
2015-01-09 18:00:07 -0600

    Solve exercise 1-7

---
+++ ch1/ex/ex1.7.c
@@ -0,0 +1,12 @@
+/*	Exercise 1.7
+ *	Write a program to print the value of EOF.
+ */
+
+ #include <stdio.h>
+
+ int main()
+ {
+ 	putchar(EOF);			// print the character
+ 	printf("\t%d\n", EOF);	// print the number
+ 	return 0;
+ }
